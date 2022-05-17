c8385995aeab5e2014547ec8acdce561ec86a4de
kbnz <9kbarker2@hbhs.school.nz>
2022-05-17 14:50:56 +1200

    started project
    started new visual studio program

---
+++ 12IA_2.8_Kyle_Barker/12IA_2.8_Kyle_Barker/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _12IA_2._8_Kyle_Barker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
